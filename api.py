b82ab8e0c9491be7c1a506404103c200af55c438
Abhay kumar singh <46368902+a8hay@users.noreply.github.com>
2020-10-14 15:26:05 +0000

    completed api

--- api.py
+++ api.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
+import io
+import json
 
 import torch
 import torchvision
@@ -21,15 +24,50 @@ def get_model():
 	model.eval()
 	print("model loaded")
 
+def preprocess_image(image_bytes, target_size):
+	image = Image.open(io.BytesIO(image_bytes))
+	if image.mode != "RGB":
+		image = image.convert("RGB")
+	transform = transforms.Compose([
+			transforms.Resize(target_size),
+			transforms.ToTensor()
+		])
+	image = transform(image).unsqueeze(dim=0)
+	return image
+
+def get_prediction(image_bytes):
+	processed_image  = preprocess_image(image_bytes, target_size=(128, 128))
+	with torch.no_grad():
+		output = model(processed_image)
+		pred = torch.argmax(output, dim=1)
+		res =  "dog" if pred.item() else "cat"
+	return res
+ALLOWED_EXTENSIONS = {"png", "jpeg", "jpg"}
+def allowed_file(filename):
+	# abc.png, abc.jpeg, abc.jpg
+	return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/')
 def hello():
     return "hiya"
 
 @app.route('/predict', methods=["POST"])
 def predict():
-    return jsonify({"cat":0, "dog":1})
+	if request.method == "POST":
+		file = request.files.get("file")
+		if file is None or file.filename == "":
+			return jsonify({"error":"no file"})
+		if not allowed_file(file.filename):
+			return jsonify({"error":"formated not supported, only jpg, png, jpeg"})
+		try:
+			img_bytes = file.read()
+			pred = get_prediction(img_bytes)
+			return jsonify({"prediction":pred})
+		except:
+			return jsonify({"error":"error during prediction process"})
+	return jsonify({"bad request":"request is not of post type"})
 
 if __name__ == "__main__":
-    print("loading pytorch modal")
-    get_model()
-    app.run(debug=False)
+	print("loading pytorch model")
+	get_model()
+	app.run(debug=False)
